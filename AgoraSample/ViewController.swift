32556e370a9d8a69e1cc40ff493ec132b6dc510f
fummicc1 <fumiya989@gmail.com>
2020-03-31 21:21:02 +0900

    fin

--- AgoraSample/ViewController.swift
+++ AgoraSample/ViewController.swift
@@ -14,7 +14,6 @@ let channelId: Int = 1
 class ViewController: UIViewController {
     
     lazy var engine = AgoraRtcEngineKit.sharedEngine(withAppId: "", delegate: self)
-    var isJoiningChannel: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +24,12 @@ class ViewController: UIViewController {
         let joinResult = engine.joinChannel(byToken: nil, channelId: String(channelId), info: nil, uid: 0, joinSuccess: nil)
         if joinResult == 0 {
             engine.enableAudio()
-            isJoiningChannel = true
         }
     }
     
     @IBAction private func didTapLeaveButton() {
         let leaveResult = engine.leaveChannel(nil)
         if leaveResult == 0 {
-            isJoiningChannel = false
         }
     }
 }
